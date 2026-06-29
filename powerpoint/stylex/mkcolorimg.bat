a85539c79075d830f3554f49db7381031067f0ff
kblee0 <kblee0@gmail.com>
2026-06-29 17:20:33 +0900

    update

--- powerpoint/stylex/mkcolorimg.bat
+++ powerpoint/stylex/mkcolorimg.bat
@@ -25,4 +25,20 @@ C:\home\local\ImageMagick\magick ^
   "color\C%%C.png"
 )
 
-copy CNONE.png color\
+C:\home\local\ImageMagick\magick ^
+  -size 32x32 canvas:none ^
+  -fill white ^
+  -draw "roundrectangle 0,0 31,31 3,3" ^
+  -fill "#808080" ^
+  -draw "roundrectangle 2,2 29,29 1,1" ^
+  -fill "#FFFFFF" ^
+  -draw "rectangle 4,4 27,27" ^
+  -fill "#ABABAB" -draw "rectangle 4,4 9,9" ^
+  -fill "#ABABAB" -draw "rectangle 16,4 21,9" ^
+  -fill "#ABABAB" -draw "rectangle 10,10 15,15" ^
+  -fill "#ABABAB" -draw "rectangle 22,10 27,15" ^
+  -fill "#ABABAB" -draw "rectangle 4,16 9,21" ^
+  -fill "#ABABAB" -draw "rectangle 16,16 21,21" ^
+  -fill "#ABABAB" -draw "rectangle 10,22 15,27" ^
+  -fill "#ABABAB" -draw "rectangle 22,22 27,27" ^
+  "color\CNONE.png"
